f2f8e7a02f0fde577e119137b51a55fc7251b1ed
Alvin Qi <peng.edy@gmail.com>
2017-03-12 22:51:34 +0800

    fix comma

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :chat_rooms, only: [:new, :create，:show, :index]
+  resources :chat_rooms, only: [:new, :create, :show, :index]
   root 'chat_rooms#index'
 
   mount ActionCable.server => '/cable'
