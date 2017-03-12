50e9c4f5f2a73e6a4aba0a64841a6a4e55a41eac
Alvin Qi <peng.edy@gmail.com>
2017-03-12 23:23:32 +0800

    cable is working, page need to fix

--- app/channels/chat_rooms_channel.rb
+++ app/channels/chat_rooms_channel.rb
@@ -1,6 +1,6 @@
 class ChatRoomsChannel < ApplicationCable::Channel
   def subscribed
-    stream_form "chat_rooms_#{params['chat_room_id']}_channel"
+    stream_from "chat_rooms_#{params['chat_room_id']}_channel"
   end
 
   def unsubscribed
@@ -9,6 +9,6 @@ class ChatRoomsChannel < ApplicationCable::Channel
 
   def send_message(data)
     # process data sent from the page
-    current_user.messages.created!(body: data['message'], chat_room_id: data['chat_room_id'])
+    current_user.messages.create!(body: data['message'], chat_room_id: data['chat_room_id'])
   end
 end
